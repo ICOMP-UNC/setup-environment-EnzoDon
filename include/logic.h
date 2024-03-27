b4c04207e8362c6f86f46bd7785823b83b96cdd7
EnzoDon <enzo.don@mi.unc.edu.ar>
2024-03-26 22:33:21 -0300

    Add documentation

--- include/logic.h
+++ include/logic.h
@@ -1,5 +1,12 @@
 
-
+/**
+ * @brief Adds two numbers.
+ * This function adds thow integers
+ * @param first_number the first number to be added.
+ * @param second_number the second number to be added.
+ * 
+ * @return the result of adds first_number and second_number
+*/
 int add(int first_number, int second_number);
 
 int substract(int first_number, int second_number);
